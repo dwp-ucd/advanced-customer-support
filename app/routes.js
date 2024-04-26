09ac0fc04ac731ddc1174a67d7dac81657643376
Allison Palum <allison.palum@engineering.digital.dwp.gov.uk>
2024-04-26 15:21:19 +0100

    update file names

--- app/routes.js
+++ app/routes.js
@@ -7,12 +7,12 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
-router.post('/acs/in_person_adjustments/v1/forms/01customer-consent', function(request, response) {
+router.post('/acs/in_person_adjustments/v1/forms/01does-the-customer-know', function(request, response) {
     var customerConsent = request.session.data['customer-consent']
     console.log(customerConsent)
     if (customerConsent == "Yes"){
         response.redirect("/acs/in_person_adjustments/v1/forms/02what-kind-of-adjustment")
     } else {
-        response.redirect("/acs/in_person_adjustments/v1/forms/03no-customer-consent")
+        response.redirect("/acs/in_person_adjustments/v1/forms/03customer-does-not-know")
     }
 })
