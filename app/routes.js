11ce33d214c037aa1b46b2c89373aea7795b8570
Alison-Lewishill <143614674+Alison-Lewishill@users.noreply.github.com>
2024-05-20 17:48:37 +0100

    Updating adjustments to show in correct categories

--- app/routes.js
+++ app/routes.js
@@ -40,8 +40,29 @@ router.post('/acs/in_person_adjustments/v2/forms/01does-the-customer-know', func
 router.post('/acs/in_person_adjustments/v3/forms/01does-the-customer-know', function(request, response) {
     var customerConsent = request.session.data['customer-consent']
     if (customerConsent == "Yes"){
-        response.redirect("/acs/in_person_adjustments/v3/forms/02what-kind-of-adjustment")
+        response.redirect("/acs/in_person_adjustments/v3/forms/03what-kind-of-adjustment")
     } else {
-        response.redirect("/acs/in_person_adjustments/v3/forms/03customer-does-not-know")
+        response.redirect("/acs/in_person_adjustments/v3/forms/05customer-does-not-know")
     }
 })
+
+// VERSION 3
+router.post('/acs/in_person_adjustments/v3/forms/02does-the-customer-know-spoken', function(request, response) {
+    var customerConsent = request.session.data['customer-consent']
+    if (customerConsent == "Yes"){
+        response.redirect("/acs/in_person_adjustments/v3/forms/04what-kind-of-spoken-adjustment")
+    } else {
+        response.redirect("/acs/in_person_adjustments/v3/forms/05customer-does-not-know")
+    }
+})
+
+router.use((req, res, next) => {
+    const log = {
+      method: req.method,
+      url: req.originalUrl, //URL of page
+      data: req.session.data //all data held
+    }
+    console.log(JSON.stringify(log, null, 2)) // show all data as a dump in terminal
+    next() // continue to next action
+  
+  })
