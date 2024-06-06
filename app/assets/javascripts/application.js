39a8a54f816d8f7c7ae12194efc3d4773073e217
kateradcliff <kate.radcliff@engineering.digital.dwp.gov.uk>
2024-06-06 17:31:43 +0100

    Add blue line for active menu item

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,16 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
+  // Add active class to the current topnav link (highlight it)
+  var topnav = document.getElementById("topnav");
+  var links = topnav.getElementsByClassName("topnav-link");
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) { 
+     current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+   });
+ }
 })
